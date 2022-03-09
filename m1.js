275993ce4cff1313e27e4eb501b4d6c1dd03b102
Mohammd-git <cdrmohammad@gmail.com>
2022-03-09 12:33:32 +0530

    added 2 files

--- m1.js
+++ m1.js
@@ -1,3 +1,4 @@
 console.log('Welcome to profile')
 console.log("hello")
 console.log("m1")
+console.log("here")
--- m2.js
+++ m2.js
@@ -3,4 +3,4 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 
-console.timeLog()
+console.log("m2")
